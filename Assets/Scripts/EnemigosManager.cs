aaf2f7553ad6e63d2fbceeb53770a602714e6aa3
santicap529 <santinocapelli2010@gmail.com>
2026-09-25 10:39:30 -0300

    25/9

---
+++ Assets/Scripts/EnemigosManager.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemigosManager : MonoBehaviour
+{
+    public EnemigoScript[] enemies;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+      enemies = FindObjectsOfType<EnemigoScript>();
+      SetAllEnemiesDamagePointsTo(5);
+      Debug.Log(enemies[enemies.Length-1].damagePoints);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void SetAllEnemiesDamagePointsTo(int value)
+    {
+      for(int i = 0 ; i < enemies.Length ; i++)
+      {
+         enemies[i].damagePoints = value;
+      }
+    }
+}
